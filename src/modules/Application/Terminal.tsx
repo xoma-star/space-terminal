52641403edaaa836dbab7faf65cb15b0e7f9755f
ae.sirotin <ae.sirotin@tensor.ru>
2024-07-23 17:07:57 +0500

    терминал на мобильной клавиатуре чек

--- src/modules/Application/Terminal.tsx
+++ src/modules/Application/Terminal.tsx
@@ -1,4 +1,4 @@
-import {KeyboardEvent, useEffect, useRef, useState} from 'react';
+import {ChangeEvent, FormEvent, KeyboardEvent, useEffect, useRef, useState} from 'react';
 import css from './Terminal.module.css';
 import useStore from '@/shared/store';
 import commandHandler from './Terminal/lib/commandHandler';
@@ -9,30 +9,32 @@ function Terminal() {
   const inputRef = useRef<HTMLInputElement>();
   const {activeWindow} = useStore();
 
-  const changeHandler = (e: KeyboardEvent) => {
-    if (e.key === 'Backspace') {
-      setInput((prevState) => prevState.slice(0, -1));
-    } else if (e.key === 'Enter') {
-      setMessages((prevState) => {
-        const newMessages = [...prevState, input];
+  const changeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setInput(value);
+  };
 
-        try {
-          commandHandler(input);
-        } catch (e: Error) {
-          newMessages.push(e.message);
-        }
+  const submitHandler = (e: FormEvent) => {
+    e.preventDefault();
+    setMessages((prevState) => {
+      const newMessages = [...prevState, input];
 
-        return newMessages;
-      });
-      setInput('');
-    } else if (e.key.length === 1) {
-      setInput((prevState) => prevState + e.key);
-    }
+      try {
+        commandHandler(input);
+      } catch (e: Error) {
+        newMessages.push(e.message);
+      }
+
+      return newMessages;
+    });
+    setInput('');
   };
 
   return (
     <div onClick={() => inputRef.current?.focus()}>
-      <input ref={inputRef} type="text" className="w-0 absolute" value={input} onKeyDown={changeHandler} />
+      <form onSubmit={submitHandler}>
+        <input ref={inputRef} type="text" className="w-0 absolute" value={input} onChange={changeHandler} />
+      </form>
       <div className="w-full h-full bg-black text-white min-h-72 cursor-default">
         {[...messages, input].map((x, i) => (
           <div key={i} className="flex flex-row">
