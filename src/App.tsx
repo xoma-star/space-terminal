dee2d3a3d166f7bad45620bdbb81f0e486417f52
ae.sirotin <ae.sirotin@tensor.ru>
2024-07-29 15:19:07 +0500

    tg

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,16 @@ import {Desktop} from './modules/Desktop';
 import {WindowsContainer} from './modules/Window';
 import {Taskbar} from './modules/Taskbar';
 import {QueryClientProvider, QueryClient} from '@tanstack/react-query';
+import WebApp from '@twa-dev/sdk';
+import {useEffect} from 'react';
 
 const queryClient = new QueryClient();
 
 export default function App() {
+  useEffect(() => {
+    WebApp.showAlert('Hey')
+  } ,[])
+
   return (
     <QueryClientProvider client={queryClient}>
       <WindowsContainer />
